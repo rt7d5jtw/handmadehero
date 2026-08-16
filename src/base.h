c3acfd2d2fb64b75ce45ab3565facc2ea5ad740d
Iia <rt7d5jtw@protonmail.com>
2026-08-16 17:21:01 +0300

    formatting for src/base.h

--- src/base.h
+++ src/base.h
@@ -5,7 +5,7 @@
 
 #pragma once
 #ifndef __BASELIB__
-#define __BASELIB__
+#  define __BASELIB__
 #endif
 
 // searchable typecast
@@ -35,10 +35,13 @@ typedef size_t usize;
 // packed structs macro
 // --------------------
 // https://stackoverflow.com/a/3312896
-// GCC  __attribute__((packed)) https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Packed-Structures.html
-// MSVC pragma pack             https://learn.microsoft.com/en-us/cpp/preprocessor/pack?view=msvc-170
+// GCC  __attribute__((packed))
+// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Packed-Structures.html
+// MSVC pragma pack
+// https://learn.microsoft.com/en-us/cpp/preprocessor/pack?view=msvc-170
 #if defined(__GNUC__) || defined(__clang__)
-#  define PACK( __Declaration__ ) __Declaration__ __attribute__((packed))
-#else //if _MSC_VER
-#  define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
+#  define PACK(__Declaration__) __Declaration__ __attribute__((packed))
+#else // if _MSC_VER
+#  define PACK(__Declaration__) \
+    __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
 #endif
